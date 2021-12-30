953d886213fcd89f8b499e7fbb65240bff7880cd
ibtihelbs <91198460+ibtihelbs@users.noreply.github.com>
2021-12-30 20:34:18 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,109 @@
+//import {hithere} from '/calculator.js';
+import Calculator from '/calculator.js';
+let Numbers=document.querySelectorAll('[data-value="number"]');
+let Operator=document.querySelectorAll('[data-value="operator"]');
+
+let Delete=document.querySelector('.DEL');
+let Resert=document.querySelector('.Resert');
+let submit=document.querySelector('#submit');
+console.log(Numbers , Operator,Delete,Resert,submit);
+
+//let Calculator=document.querySelector(".calculator");
+let Result=document.querySelector(".Results h1");
+
+const _Calculator=new Calculator(Result,"");
+
+Resert.addEventListener('click',()=>{
+   _Calculator.Resert();
+   _Calculator.updateDisplay();
+})
+Numbers.forEach((number) => {
+  number.addEventListener("click",(button)=>{
+     //console.log(number);
+     
+    _Calculator.appendNumber(number.innerHTML);
+    _Calculator.updateDisplay();
+    //alert('hiihihii');
+})
+})
+
+Operator.forEach((number) => {
+   number.addEventListener("click",(button)=>{
+      //console.log(number);
+      
+     _Calculator.operator(number.innerHTML);
+     _Calculator.updateDisplay();
+     //alert('hiihihii');
+  })
+ })
+
+submit.addEventListener('click',(button)=>{
+   //alert(button);
+   _Calculator.calc();
+   _Calculator.updateDisplay();
+
+})
+Delete.addEventListener('click',(button)=>{
+   
+   _Calculator.Delete();
+   _Calculator.updateDisplay();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**theme swiching */
+let the_swisher=document.querySelector('.the-swisher');
+
+the_swisher.addEventListener("click",(f)=>{
+    f.preventDefault();
+    let currently_active =document.querySelector('.currently-active');
+    let li=f.target.closest("li");
+    if(!li)return;
+    let attr=li.getAttribute('data-attribution');
+    let _body=document.querySelector("body");
+    let _class = _body.getAttribute('class');
+    const _themeSwitcher=(attrName,className)=>{
+        if(attr==attrName){
+        
+            _body.classList.remove(_class);
+            _body.classList.toggle(className);
+         }
+    }
+    _themeSwitcher(1,'first-theme');
+    _themeSwitcher(2,'second-theme');
+    _themeSwitcher(3,'third-theme');
+    /*if(attr==1){
+        
+       _body.classList.remove(_class);
+       _body.classList.toggle('first-theme');
+    }
+    if(attr==2){
+        //let _body=document.querySelector("body");
+        //let _class = _body.getAttribute('class');
+        _body.classList.remove(_class);
+        _body.classList.toggle('second-theme');
+     }
+     if(attr==3){
+        //let _body=document.querySelector("body");
+        //let _class = _body.getAttribute('class');
+        _body.classList.remove(_class);
+        _body.classList.toggle('second-three');
+     }*/
+    li.classList.toggle("currently-active");
+    currently_active.classList.remove("currently-active");
+    
+})
---
+++ calculator.js
@@ -0,0 +1,105 @@
+export default class Calculator {
+
+    constructor(outPut,latestOuput){
+      this.outPut=outPut;
+      this.latestOuput=latestOuput;
+      this.Resert();
+    }
+    
+    Resert(){
+       
+        this.currentOutput='';
+        this._operator='';
+    }
+
+    Delete(){
+        this.currentOutput=this.currentOutput.toString().slice(0,-1);
+    }
+
+    equal(){}
+
+    appendNumber(number){
+        if(number === "." && this.currentOutput.includes("."))return;
+        this.currentOutput = this.currentOutput.toString()+number.toString();
+        
+    }
+    
+    operator(operator){
+        if(this.currentOutput=='')return;
+        if(this.latestOuput !==''){
+            this.calc();
+        }
+        this._operator=operator;
+        this.latestOuput=this.currentOutput;
+        this.currentOutput='';
+    }
+
+    calc(){
+        let camputation;
+        const current=parseFloat(this.currentOutput);
+        const previou=parseFloat(this.latestOuput);
+        console.log(current,previou);
+        if(isNaN(current)||isNaN(previou))return;
+        switch(this._operator){
+            case '+':
+            camputation = current + previou;
+            break;
+
+            case '-':
+            camputation = current - previou;
+            break;
+            
+            case 'X':
+            camputation = current * previou;
+            break;
+            
+            case '/':
+            camputation = current / previou;
+            break;
+            
+            default:
+                return;
+
+        }
+        this.currentOutput=camputation;
+        this._operator=undefined;
+        this.latestOuput='';
+    }
+
+    updateDisplay(){
+        console.log(this.currentOutput);
+        this.outPut.innerHTML=this.currentOutput;
+    }
+
+    Display(){
+        
+        
+    }
+}
+
+//export default _Calculator();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export function hithere(){
+    alert('hi there');
+}
